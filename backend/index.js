210adce2f7551e2016daa0e4eaf499d92f9885af
jmoral8 <jaimmoral98@gmail.com>
2025-06-30 12:43:01 -0500

    add a new page

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,7 @@ import express from 'express';
 
 const PORT = 3001;
 const app = express();
-
 app.get("bruh")
-
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
---
+++ frontend/src/app/NewPage.tsx
@@ -0,0 +1,7 @@
+export const NewPage = () => {
+    return(
+        <>
+            This is my new page
+        </>
+    )
+}
