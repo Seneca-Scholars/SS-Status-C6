00aed11a18d7dc1f1ef10d2427d9377f4dac6f66
jmoral8 <jaimmoral98@gmail.com>
2025-06-30 11:48:20 -0500

    add more content on the server and on changed the title

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,9 @@ import express from 'express';
 
 const PORT = 3001;
 const app = express();
+
 app.get("bruh")
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
